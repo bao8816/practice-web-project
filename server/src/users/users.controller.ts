2473dad8e116c1dd3bd0511454eb65d9f4b3ae06
Le Thai Phuc Bao <nile1087@gmail.com>
2025-04-02 20:58:35 +0700

    server: refactor code

--- server/src/users/users.controller.ts
+++ server/src/users/users.controller.ts
@@ -1,6 +1,5 @@
-import { Body, Controller, Get, Param, Post } from '@nestjs/common';
+import { Controller, Get, Param } from '@nestjs/common';
 import { UsersService } from './users.service';
-import { Users } from './users.entity';
 
 @Controller('users')
 export class UsersController {
