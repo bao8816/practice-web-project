52dc2be51dfe983695fe917ee197b862f9178933
Le Thai Phuc Bao <nile1087@gmail.com>
2025-10-19 16:27:51 +0700

    profiles: re-config prettier and eslint

--- client/eslint.config.js
+++ client/eslint.config.js
@@ -6,7 +6,7 @@ import tseslint from 'typescript-eslint';
 import prettierConfig from 'eslint-config-prettier';
 
 export default tseslint.config(
-    { ignores: ['dist'] },
+    { ignores: ['dist', 'node_modules', 'build', 'coverage', '.vite'] },
     {
         extends: [js.configs.recommended, ...tseslint.configs.recommended],
         files: ['**/*.{js,ts,tsx}'],
@@ -23,5 +23,5 @@ export default tseslint.config(
             'react-refresh/only-export-components': ['warn', { allowConstantExport: true }],
         },
     },
-    prettierConfig,
+    prettierConfig
 );
