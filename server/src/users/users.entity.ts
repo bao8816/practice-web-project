ccd34e12a23096f710d66332f8160d47020d1fa0
Le Thai Phuc Bao <nile1087@gmail.com>
2025-12-06 22:12:32 +0700

    addresses: optimize entity relations

--- server/src/users/users.entity.ts
+++ server/src/users/users.entity.ts
@@ -1,4 +1,14 @@
-import { Entity, PrimaryGeneratedColumn, Column, CreateDateColumn, UpdateDateColumn } from 'typeorm';
+import { Addresses } from 'src/addresses/addresses.entity';
+import { Profiles } from 'src/profiles/profiles.entity';
+import {
+    Entity,
+    PrimaryGeneratedColumn,
+    Column,
+    CreateDateColumn,
+    UpdateDateColumn,
+    OneToMany,
+    OneToOne,
+} from 'typeorm';
 
 @Entity()
 export class Users {
@@ -16,6 +26,12 @@ export class Users {
 
     @Column({ type: 'varchar', length: 255, nullable: true, default: null })
     email: string | null;
+
+    @OneToOne(() => Profiles, (profile) => profile.user)
+    profile: Profiles;
+
+    @OneToMany(() => Addresses, (address) => address.user)
+    addresses: Addresses[];
 
     @CreateDateColumn()
     createdAt: Date;
